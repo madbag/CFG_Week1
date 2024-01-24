de45715386dc5065e2b3bd4f34aff76bd7da25d2
Madhushree B <boylemadhushree@gmail.com>
2024-01-24 21:12:54 +0100

    "First day of Python"

---
+++ main.py
@@ -0,0 +1,49 @@
+# The string Type Data
+# print("Cat")
+# print("Cat" + "videos")
+# print("Cat" * 3)
+# print("Cat" + str(3))
+# print("Cat".upper())
+# print("Cat".lower())
+# print("the lord of the rings".title())
+
+# # Variables
+# print("spaghetti")
+# output="spaghetti"
+# print(output)
+# oranges= 12
+# cost_per_orange= 0.5
+# total_cost= oranges * cost_per_orange
+# print(str(oranges)+ " " +"oranges")
+# print("cost" + str(total_cost))
+# # Exe 1.4
+# cats= 10
+# cans= 2
+# days= 7
+# total_cans = cats * cans * days
+# output = str(cats) + " cats eat " + str(total_cans) + " cans in " + str(days) + " days"
+# print(output)
+
+# String Formatting
+# oranges = 12
+# cost_per_orange= 0.5
+# total_cost = oranges * cost_per_orange
+# output="{} oranges costs Pounds {}".format(oranges, total_cost)
+# print(output)
+# Exercise 1.5:
+# user_name = 'sarah_1987'
+# age = 23
+# output = '{} is {} years old'.format(user_name, age)
+# print(output)
+# cats = 10
+# cans = 2
+# total_cans = cats * cans
+# output = "{} cats eat {} cans".format(cats, total_cans)
+# print(output)
+
+# Recap
+days = 31
+hours = 24
+total_hours = days * hours
+msg = "There are {} in {} days".format(total_hours, days)
+print(msg)
